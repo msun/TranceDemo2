7c8984e24ad9fa4431019001431cfc8edddb6af6
Matthew Sun <matthew@luna.local>
2015-10-13 18:47:44 -0400

    basic get request for json

--- TranceDemo2/TRFeedViewController.m
+++ TranceDemo2/TRFeedViewController.m
@@ -7,13 +7,51 @@
 //
 
 #import "TRFeedViewController.h"
+#import <Foundation/Foundation.h>
 
 @implementation TRFeedViewController
+
+- (void)updateData {
+    NSString *urlString = @"https://tranceapp.com/_utils/getData.php";
+    
+    NSURLSession *session = [NSURLSession sharedSession];
+    [[session dataTaskWithURL:[NSURL URLWithString:urlString]
+            completionHandler:^(NSData *data,
+                                NSURLResponse *response,
+                                NSError *error) {
+        if (error != nil) {
+            NSLog(@"GET Request error");
+            return;
+        } else if (data == nil) {
+            NSLog(@"No data received");
+            return;
+        }
+
+        NSError *jsonError = nil;
+        id jsonObject = [NSJSONSerialization JSONObjectWithData:data
+                                                    options:0
+                                                      error:&jsonError];
+        if (jsonError) {
+            NSLog(@"JSON was malformed");
+            return;
+        }
+
+        if([jsonObject isKindOfClass:[NSArray class]]) {
+            NSArray *results = jsonObject;
+            NSLog(@"ok");
+        } else {
+            NSLog(@"It wasn't an array");
+            return;
+        }
+    }] resume];
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     
     //self.tableView.backgroundColor = [UIColor redColor];
+    
+    [self updateData];
 }
 
 # pragma mark -
